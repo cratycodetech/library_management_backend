d4f3348a43271451e452a6dccdbce04ec4cb1fdc
AminulSagor <aminulislamsagor3@gmail.com>
2025-04-21 12:45:41 +0600

    Setup backend with NestJS, TypeORM, PostgreSQL, and Agora token service

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,18 +1,11 @@
 import * as admin from 'firebase-admin';
-import * as fs from 'fs';
-import * as path from 'path';
-
-
-const serviceAccountPath = path.join(__dirname, '../../message-notification-830cc-firebase-adminsdk-cwytn-b5811c6135.json');
-
-if (!fs.existsSync(serviceAccountPath)) {
-  throw new Error(`Service account file not found at ${serviceAccountPath}`);
-}
-
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 export { admin };
