cc5e9a223828f2a06ce380e9f773c4ebf470ba46
AminulSagor <aminulislamsagor3@gmail.com>
2025-04-21 15:09:02 +0600

    ✅ Refactor Firebase config to use single service account env

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,11 +1,9 @@
 import * as admin from 'firebase-admin';
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  }),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export { admin };
